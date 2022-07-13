2f18e974db5b31a460d64da9f8fc6989e19796f4
li <853312951@qq.com>
2022-07-13 12:13:14 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int number=scanner.nextInt();
+        if (number%3==0&&number%5==0)
+            System.out.println("FizzBuzz");
+        else if (number%3==0)
+            System.out.println("Fizz");
+        else if (number%5==0)
+            System.out.println("BUzz");
+        else
+            System.out.println("number");
+    }
+}
